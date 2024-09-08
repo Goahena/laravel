d72d446da42fefe2f1e1fe748d9d0b687aacc1f3
Goahena <trunghau190303@gmail.com>
2024-09-08 17:11:40 +0700

    add dashboard, login and logout

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Backend\AuthController;
+use App\Http\Controllers\Backend\DashboardController;
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\AuthenticateMiddleware;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('admin', [AuthController::class, 'index'])->name('auth.admin');
+Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(AuthenticateMiddleware::class);
